b82146d0f2c414e51272918513f778ed1b69a45a
Chad McLeavy <chad_mcleavy@hotmail.com>
2020-06-24 20:25:27 -0700

    - Created blinking slime

---
+++ Assets/Resources/Scripts/SlimeFaceTestScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeFaceTestScript : MonoBehaviour
+{
+    public TextureSheetAnimator faceAnimator;
+    public TextureSheetAnimation blinkAnim;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            faceAnimator.animateTextureSheet(blinkAnim);
+    }
+}
